356f725ea13f161bd3993bec325ec2603e58ab50
Salvatore Torcivia <sal@saltor.nyc>
2017-03-12 16:26:23 -0400

    Update inspiration and proficiency bonus display

--- development/js/components/Stats.vue
+++ development/js/components/Stats.vue
@@ -18,14 +18,22 @@
                             </div>
 
                             <div class="modifiers-container">
+                                <div class="modifier modifier_inspiration">
+                                    <div class="modifier__value modifier__value_inspiration">{{ user.inspiration_modifier | sign }}</div>
+                                    <div class="modifier__name modifier__name_inspiration">Inspiration</div>
+                                </div>
 
+                                <div class="modifier modifier_proficiency">
+                                    <div class="modifier__value modifier__value_proficiency">{{ user.proficiency_bonus | sign }}</div>
+                                    <div class="modifier__name modifier__name_proficiency">Proficiency Bonus</div>
+                                </div>
                             </div>
                         </div>
 
                         <div class="modifier modifier__passive-wisdom"></div>
                     </div>
 
-                    <!--<div>Hitpoints: {{user.hitpoints.max}}/{{user.hitpoints.current}}</div>-->
+                    <div>Hitpoints: {{ user.hitpoints.max }}/{{ user.hitpoints.current }}</div>
 
                     <div>Speed: {{user.speed}}</div>
 
@@ -84,9 +92,7 @@
         props: ["user"],
         created() {
             Vue.filter('sign', number => number > 0 ? `+${number}` : `${number}`)
-            Vue.filter('modifier', saving_throw => {
-                return this.user.saving_throw_modifiers[saving_throw]
-            })
+            Vue.filter('modifier', saving_throw => this.user.saving_throw_modifiers[saving_throw])
         },
         data() {
             return {}
