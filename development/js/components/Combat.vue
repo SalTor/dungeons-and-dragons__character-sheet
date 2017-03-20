8b11792865987d17c1c1fb0dbec2d4411830f2ca
Salvatore Torcivia <sal@saltor.nyc>
2017-03-19 21:17:15 -0400

    Update combat page for miscellaneously charged items

--- development/js/components/Combat.vue
+++ development/js/components/Combat.vue
@@ -1,14 +1,33 @@
 <template>
     <main role="main">
         <section>
-            <div class="section__content" v-if="!loading">
+            <div class="section__content">
                 <div class="section__description">
+                    <div class="miscellaneous-charges">
+                        <div class="miscellaneous-charges__title">Miscellaneous Items With Charges</div>
+
+                        <div class="miscellaneous-items">
+                            <div class="miscellaneous-item" v-for="item in user.weapons_with_charges">
+                                <div class="miscellaneous-item__name">{{ item.name }}</div>
+
+                                <div class="miscellaneous-item__charges">
+                                    <div class="miscellaneous-item__charge-count-modifiers">
+                                        <i class="fa fa-refresh miscellaneous-item__charge-count-modifier"  @click="reset_charges(item)"  :class="item.charges.expended === 0 ? 'miscellaneous-item__charge-count-modifier_disabled' : ''"></i>
+                                        <i class="fa fa-minus   miscellaneous-item__charge-count-modifier"  @click="restore_charge(item)" :class="item.charges.expended === 0 ? 'miscellaneous-item__charge-count-modifier_disabled' : ''"></i>
+                                        <i class="fa fa-plus    miscellaneous-item__charge-count-modifier"  @click="expend_charge(item)"  :class="item.charges.expended === item.charges.total ? 'miscellaneous-item__charge-count-modifier_disabled' : ''"></i>
+                                    </div>
+                                    <div class="miscellaneous-item__charge" :class="index <= item.charges.expended ? 'miscellaneous-item__charge_expended' : ''" v-for="(index, charge) in item.charges.total"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="spell-book">
                         <div class="spell-book__cantrips" v-for="(cantrips, category) in user.spell_book" v-if="category === 'cantrips'">
                             <div class="cantrips__title">Cantrips:</div>
 
                             <div class="cantrips__spells">
-                                <div class="cantrip" v-for="cantrip in cantrips">{{cantrip}}</div>
+                                <div class="cantrip" v-for="cantrip in cantrips">{{ cantrip }}</div>
                             </div>
                         </div>
 
@@ -17,7 +36,7 @@
                                 <div class="spell-tier__title">
                                     <span>Level {{ category }}</span>
                                     <span class="spell-tier__slot-modifiers">
-                                        <i class="fa fa-minus spell-tier__slot-modifier spell-tier__slot-modifier_renew-slot"   @click="renew_spell_slot(details)"  :class="details.slots.expended === 0 ? 'spell-tier__slot-modifier_disabled' : ''"></i>
+                                        <i class="fa fa-minus spell-tier__slot-modifier spell-tier__slot-modifier_renew-slot"   @click="restore_spell_slot(details)"  :class="details.slots.expended === 0 ? 'spell-tier__slot-modifier_disabled' : ''"></i>
                                         <i class="fa fa-plus  spell-tier__slot-modifier"   @click="expend_spell_slot(details)" :class="details.slots.expended === details.slots.total ? 'spell-tier__slot-modifier_disabled' : ''"></i>
                                     </span>
                                 </div>
@@ -47,13 +66,9 @@
         name: "combat",
         props: [ "user" ],
         data(){
-            return {
-                loading: true
-            }
+            return {}
         },
-        created() {
-            this.loading = false
-        },
+        created() {},
         mounted() {},
         methods: {
             spellbook(category, book) {
@@ -67,13 +82,26 @@
                     details.slots.expended++
                 }
             },
-            renew_spell_slot(details) {
+            restore_spell_slot(details) {
                 if(details.slots.expended - 1 >= 0) {
                     details.slots.expended--
                 }
             },
             reset_spell_slots(details) {
                 details.slots.expended = 0
+            },
+            expend_charge(item) {
+                if(item.charges.expended + 1 <= item.charges.total) {
+                    item.charges.expended++
+                }
+            },
+            restore_charge(item) {
+                if(item.charges.expended - 1 >= 0) {
+                    item.charges.expended--
+                }
+            },
+            reset_charges(item) {
+                item.charges.expended = 0
             }
         }
     }
