60cc05df52e26b38b86267dde9284f808f972b1e
Salvatore Torcivia <sal@saltor.nyc>
2017-10-01 13:48:01 -0400

    App shell: Convert " to '

--- source/js/components/App.vue
+++ source/js/components/App.vue
@@ -9,18 +9,18 @@
 </template>
 
 <script>
-    import Vue from "vue"
-    import Navigation from "./Navigation.vue"
-    import Footer from "./Footer.vue"
+    import Vue from 'vue'
+    import Navigation from './Navigation.vue'
+    import Footer from './Footer.vue'
 
-    require("promise-polyfill")
-    require("fetch-ie8")
+    require('promise-polyfill')
+    require('fetch-ie8')
 
-    Vue.component("app-navigation", Navigation)
-    Vue.component("app-footer", Footer)
+    Vue.component('app-navigation', Navigation)
+    Vue.component('app-footer', Footer)
 
     export default {
-        name: "app",
+        name: 'app',
         mounted() {
             this.fetch_user_data()
         },
@@ -32,7 +32,7 @@
         },
         methods: {
             fetch_user_data() {
-                fetch(`./data/character_sheet__player_name.json`)
+                fetch('./data/character_sheet__player_name.json')
                     .then(response => response.json())
                     .then(data => {
                         this.character = data[0]
@@ -43,12 +43,12 @@
         },
         created() {
             // Filters
-            Vue.filter("number", entry => entry.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
-            Vue.filter("sign", number => number >= 0 ? `+${number}` : `${number}`)
-            Vue.filter("modifier", saving_throw => this.character.saving_throw_modifiers[saving_throw])
+            Vue.filter('number', entry => entry.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))
+            Vue.filter('sign', number => number >= 0 ? `+${ number }` : `${ number }`)
+            Vue.filter('modifier', saving_throw => this.character.saving_throw_modifiers[saving_throw])
 
             // Directives
-            Vue.directive("focus", {
+            Vue.directive('focus', {
                 inserted: el => el.focus()
             })
         }
