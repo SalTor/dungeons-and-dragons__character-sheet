676647f1abdb5d7af22b32375d44323beec75823
Salvatore Torcivia <sal@saltor.nyc>
2017-10-01 13:46:06 -0400

    Build process: Remove logging from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,26 @@
-"use strict";
+const gulp        = require('gulp')
+const rename      = require('gulp-rename')
+const json_minify = require('gulp-json-minify')
+const browserSync = require('browser-sync')
 
-let gulp         = require("gulp"),
-    rename       = require("gulp-rename"),
-    json_minify  = require("gulp-json-minify"),
-    browserSync  = require("browser-sync")
+gulp.task('default', [ 'copy_watch' ])
+gulp.task('release', [ 'copy' ])
+gulp.task('dev',     [ 'copy_watch' ])
+gulp.task('server',  [ 'copy', 'browserSync' ])
 
-gulp.task("default", [ "copy", "confirmation" ])
-gulp.task("release", [ "default" ])
-gulp.task("dev",     [ "copy_watch" ])
-gulp.task("server",  [ "copy", "browserSync" ])
-
-gulp.task("copy", function () {
-    gulp.src("source/js/example_response.json", { base: process.cwd() })
-        .pipe(rename("character_sheet__player_name.json"))
+gulp.task('copy', function () {
+    gulp.src('source/js/example_response.json', { base: process.cwd() })
+        .pipe(rename('character_sheet__player_name.json'))
         .pipe(json_minify())
-        .pipe(gulp.dest("public/data"))
+        .pipe(gulp.dest('public/data'))
 })
 
-gulp.task("copy_watch", [ "copy" ], function () {
-    gulp.watch(["source/js/example_response.json"], ["copy"])
+gulp.task('copy_watch', [ 'copy' ], function () {
+    gulp.watch(['source/js/example_response.json'], ['copy'])
 })
 
-gulp.task("browserSync", [], function () {
+gulp.task('browserSync', [], function () {
     browserSync.init({
-        server: { baseDir: "./public" }
+        server: { baseDir: './public' }
     });
-})
-
-
-
-let console_spacer = `[        ]`,
-    console_linebreak = `=============================================================`
-
-gulp.task("confirmation", [], function () {
-    console.log(`${console_spacer}`)
-    console.log(`${console_spacer} ${console_linebreak}`)
-    console.log(`${console_spacer}`)
-    console.log(`${console_spacer} \tSample data is available and the app has been built`)
-    console.log(`${console_spacer}`)
-    console.log(`${console_spacer} ${console_linebreak}`)
-    console.log(`${console_spacer}`)
 })
