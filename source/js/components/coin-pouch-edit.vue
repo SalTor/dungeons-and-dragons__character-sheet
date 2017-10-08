923e983d3023c25e34c703a4c97a730d4b5648a8
Salvatore Torcivia <sal@saltor.nyc>
2017-10-08 12:58:20 -0400

    Coin pouch: Enforce minimum value of 0 coins

--- source/js/components/coin-pouch-edit.vue
+++ source/js/components/coin-pouch-edit.vue
@@ -3,35 +3,36 @@
         <form>
             <div class="prompt__coins">
                 <div class="coin">
-                    <input v-model="copper" id="coins_copper" type="number" title="Copper coins" min="0">
+                    <input v-model="copper" id="coins_copper" type="number" title="Copper coins" min="0" required>
                     <label for="coins_copper">copper</label>
                 </div>
                 <div class="coin">
-                    <input v-model="silver" id="coins_silver" type="number" title="Silver coins" min="0">
+                    <input v-model="silver" id="coins_silver" type="number" title="Silver coins" min="0" required>
                     <label for="coins_silver">silver</label>
                 </div>
                 <div class="coin">
-                    <input v-model="electrum" id="coins_electrum" type="number" title="Electrum coins" min="0">
+                    <input v-model="electrum" id="coins_electrum" type="number" title="Electrum coins" min="0" required>
                     <label for="coins_electrum">electrum</label>
                 </div>
                 <div class="coin">
-                    <input v-model="gold" id="coins_gold" type="number" title="Gold coins" min="0">
+                    <input v-model="gold" id="coins_gold" type="number" title="Gold coins" min="0" required>
                     <label for="coins_gold">gold</label>
                 </div>
                 <div class="coin">
-                    <input v-model="platinum" id="coins_platinum" type="number" title="Platinum coins" min="0">
+                    <input v-model="platinum" id="coins_platinum" type="number" title="Platinum coins" min="0" required>
                     <label for="coins_platinum">platinum</label>
                 </div>
             </div>
 
             <div class="prompt__controls">
-                <button @click="save" class="control cta" :class="{ disabled: !any_changes }" :title="save_hover_text" type="button">save</button>
+                <button @click="save" class="control cta" :class="{ disabled: !any_changes }" :title="save_hover_text" type="button">{{ btn_text_save }}</button>
             </div>
         </form>
     </div>
 </template>
 
 <script>
+    import { isNumeric } from '../helper-functions'
     import { mixin as clickaway } from 'vue-clickaway'
 
     export default {
@@ -40,11 +41,19 @@
         props: { coins: { type: Object, required: true } },
         computed: {
             any_changes() {
-                const coins = Object.keys(this.coins)
-                const mismatches = coins.filter(coin => parseInt(this[coin]) !== parseInt(this.coins[coin]))
+                const mismatch = coin => parseInt(this[coin]) !== parseInt(this.coins[coin])
 
-                return mismatches.length > 0
+                return Object.keys(this.coins)
+                    .some(mismatch)
             }
+            , btn_text_save() { return this.any_changes ? 'save' : 'no changes' }
+        },
+        watch: {
+            copper(coin) {   this.copper   = isNumeric(parseInt(coin)) ? coin : '0' },
+            silver(coin) {   this.silver   = isNumeric(parseInt(coin)) ? coin : '0' },
+            electrum(coin) { this.electrum = isNumeric(parseInt(coin)) ? coin : '0' },
+            gold(coin) {     this.gold     = isNumeric(parseInt(coin)) ? coin : '0' },
+            platinum(coin) { this.platinum = isNumeric(parseInt(coin)) ? coin : '0' }
         },
         data() {
             const coins = {
