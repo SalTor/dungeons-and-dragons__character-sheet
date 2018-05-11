bf8f18a248ee9f59be89826b9e575384d1ce3905
SalTor <sal@saltor.nyc>
2018-05-10 20:21:54 -0400

    SpellBook: Aggregate cantrips + spells into one, alphabetical list

--- source/js/components/Spellbook.vue
+++ source/js/components/Spellbook.vue
@@ -3,6 +3,12 @@
         <section>
             <div class="section__content">
                 <h1>Spellbook</h1>
+
+                <ol>
+                    <li v-for="spell in spells">
+                        <div class="spell-book__spell --spell-book">{{ spell }}</div>
+                    </li>
+                </ol>
             </div>
         </section>
     </main>
@@ -13,9 +19,44 @@
 
     export default {
         name: "spellbook",
-        props: ["user"],
+        props: {
+            user: {
+                type: Object,
+                default() {
+                    return {
+                        spell_book: {
+                            cantrips: [],
+                            spells: {}
+                        }
+                    }
+                }
+            }
+        },
+        computed: {
+            spells() {
+                const spell_book = this.user.hasOwnProperty('spell_book')
+
+                return spell_book ? this.aggregate_spells() : null
+            },
+        },
         data() {
             return {}
-        }
+        },
+        methods: {
+            aggregate_spells() {
+                const spell_book = this.user.spell_book.spells
+                const categories = Object.keys(spell_book)
+                const spells = this.user.spell_book.cantrips
+
+                categories.forEach((cat_id) => {
+                    const category = spell_book[cat_id].entries
+                    const spell_names = category.map(({ name }) => name)
+
+                    spells.push(...spell_names)
+                })
+
+                return spells.sort()
+            },
+        },
     }
 </script>
